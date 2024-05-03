761045522e4c611b64b6777b2e3f02a1750e8bcc
Nicky Dover <dovern42@gmail.com>
2024-05-03 06:20:25 -0400

    add date

--- src/containers/PostCard.js
+++ src/containers/PostCard.js
@@ -3,6 +3,7 @@ function PostCard(post){
   return <div className="post-card">
     <div className="full-name"><span className="name">{post.username}</span> of <span className="city">{post.location}</span></div>
     <div className="post">{post.content}</div>
+    <div className="date">Anno Domini 1310</div>
   </div>
 }
 export default PostCard;
