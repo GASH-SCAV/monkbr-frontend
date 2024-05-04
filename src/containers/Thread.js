dd034eb91b5d32ecf9f19aa9b56ac4ac1505cf14
Nicky Dover <dovern42@gmail.com>
2024-05-04 16:01:37 -0400

    condemns

--- src/containers/Thread.js
+++ src/containers/Thread.js
@@ -14,15 +14,46 @@ function Thread({goToFeed, incrementToRefresh}){
     fetchPosts()
   }, [incrementToRefresh])
 
+  const changeThreadLikes = (postId, likesCount) => {
+    const newPost = {...post}
+    newPost.sanctifies = likesCount.likes
+    setPost(newPost)
+  }
+
+  const changeReplyLikes = (postId, likesCount) => {
+    const newPost = {...post}
+    const replies = [...post.replies]
+    const index = replies.findIndex(obj => obj.id == postId);
+    replies[index].sanctifies = likesCount.likes
+    newPost.replies = replies
+    setPost(newPost)
+  }
+
+  const changeThreadCondemns = (postId, condemnsCount) => {
+    const newPost = {...post}
+    newPost.condemns = condemnsCount.condemns
+    setPost(newPost)
+  }
+
+  const changeReplyCondemns = (postId, condemns) => {
+    const newPost = {...post}
+    const replies = [...post.replies]
+    const index = replies.findIndex(obj => obj.id == postId);
+    replies[index].condemns = condemns.condemns
+    newPost.replies = replies
+    setPost(newPost)
+  }
+
+
   return <main className="posts">
     <div className="reply-to">Reply to...</div>
-    <PostCard post={post} key={post.id} hideMarginaliaButton={true} />
+    <PostCard post={post} key={post.id} hideMarginaliaButton={true} changeLikes={changeThreadLikes} changeCondemns={changeThreadCondemns} />
     <Link className="child-button" to={"/"}>⬴Go Back</Link>
     <button className="child-button" onClick={() => {
       navigator.clipboard.writeText(window.location.href)
       alert("Link Copied")
       }}>Copy Link</button>
-    {post.replies.map(post => <PostCard post={post} key={post.id} hideMarginaliaButton={true} />)}
+    {post.replies.map(post => <PostCard post={post} key={post.id} hideMarginaliaButton={true} changeLikes={changeReplyLikes} changeCondemns={changeReplyCondemns} />)}
   </main>
 }
 
